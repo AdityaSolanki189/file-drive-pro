f115d008b8856788988181707b78dcb155e16092
Aditya Solanki <adityasolanki@Adis-MacBook-Air.local>
2024-06-09 04:12:12 +0530

    initial clerk auth and convex schema setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+//giving-rodent-62.clerk.accounts.dev
+
+export default {
+    providers: [
+        {
+            domain: 'https://giving-rodent-62.clerk.accounts.dev',
+            applicationID: 'convex'
+        }
+    ]
+};
