a75f274a142378464cd70b9d6d49f8ab296e4a37
Aditya Solanki <adityasolanki@Adis-MacBook-Air.local>
2024-06-09 14:59:53 +0530

    minor fixes

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,13 +1,20 @@
-import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { OrganizationSwitcher, SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
 
 export function Header() {
     return (
-        <div className="border-b">
-            <div className="container mx-auto p-4 flex justify-between items-center">
+        <div className="border-b py-4 bg-gray-50">
+            <div className="container mx-auto flex justify-between items-center">
                 <h1 className="text-2xl font-bold">FileDrivePro 🚀</h1>
                 <div className="flex gap-2">
                     <OrganizationSwitcher/>
                     <UserButton/>
+
+                    <SignedOut>
+                        <SignInButton>
+                            <Button>Sign In</Button>
+                        </SignInButton>
+                    </SignedOut>
                 </div>
             </div>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,17 +25,7 @@ export default function Home() {
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <SignedIn>
-                <SignOutButton>
-                    <Button>Sign Out</Button>
-                </SignOutButton>
-            </SignedIn>
-            <SignedOut>
-                <SignInButton mode="modal">
-                    <Button>Sign In</Button>
-                </SignInButton>
-            </SignedOut>
-
+            
             {files?.map((file) => <div key={file._id}>{file.name}</div>)}
 
             <Button
