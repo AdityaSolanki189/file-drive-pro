20f631d5f9663f0eb04efd7e37bc59fe5560a353
Aditya Solanki <adityasolanki@Adis-MacBook-Air.local>
2024-06-12 23:08:52 +0530

    add table layout for file view

---
+++ src/app/(dashboard)/_components/file-actions.tsx
@@ -0,0 +1,144 @@
+import { Doc } from '../../../../convex/_generated/dataModel';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle
+} from '@/components/ui/alert-dialog';
+import { useToast } from '@/components/ui/use-toast';
+import {
+    DownloadIcon,
+    MoreVertical,
+    StarIcon,
+    TrashIcon,
+    UndoIcon
+} from 'lucide-react';
+import { useState } from 'react';
+import { useMutation } from 'convex/react';
+import { api } from '../../../../convex/_generated/api';
+import { Protect } from '@clerk/nextjs';
+
+interface FileCardActionsProps {
+    file: Doc<'files'>;
+    isFavorite: boolean;
+}
+
+export function FileCardActions({ file, isFavorite }: FileCardActionsProps) {
+    const deleteFile = useMutation(api.files.deleteFile);
+    const restoreFile = useMutation(api.files.restoreFile);
+    const { toast } = useToast();
+    const [isConfirmOpen, setIsConfirmOpen] = useState(false);
+    const toggleFavorite = useMutation(api.files.toggleFavorite);
+
+    return (
+        <>
+            <AlertDialog open={isConfirmOpen} onOpenChange={setIsConfirmOpen}>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>
+                            Are you absolutely sure?
+                        </AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This action will mark the file for deletion. It will
+                            be moved to the Trash bin. And it will be
+                            permanently deleted after 30 days.
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction
+                            onClick={async () => {
+                                await deleteFile({
+                                    fileId: file._id
+                                });
+                                toast({
+                                    variant: 'default',
+                                    title: 'File marked for deletion',
+                                    description:
+                                        'Your file is now moved to the Trash bin. It will be permanently deleted after 30 days.'
+                                });
+                            }}
+                        >
+                            Continue
+                        </AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <MoreVertical />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuItem
+                        onClick={() => {
+                            window.open('', '_blank');
+                        }}
+                        className="flex gap-1 items-center cursor-pointer"
+                    >
+                        <DownloadIcon className="w-4 h-4" /> Download
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                        onClick={() => {
+                            toggleFavorite({
+                                fileId: file._id
+                            });
+                        }}
+                        className="flex gap-2 cursor-pointer justify-start items-center"
+                        disabled={file.shouldDelete}
+                    >
+                        {isFavorite ? (
+                            <>
+                                <StarIcon
+                                    className="w-4 h-4 text-yellow-300"
+                                    fill="yellow"
+                                />
+                                Unfavorite
+                            </>
+                        ) : (
+                            <>
+                                <StarIcon className="w-4 h-4" />
+                                Favorite
+                            </>
+                        )}
+                    </DropdownMenuItem>
+                    <Protect role="org:admin" fallback={<></>}>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem
+                            onClick={() => {
+                                if (file.shouldDelete) {
+                                    restoreFile({
+                                        fileId: file._id
+                                    });
+                                } else {
+                                    setIsConfirmOpen(true);
+                                }
+                            }}
+                        >
+                            {file.shouldDelete ? (
+                                <div className="flex gap-2 text-green-600 items-center cursor-pointer">
+                                    <UndoIcon className="w-4 h-4" /> Restore
+                                </div>
+                            ) : (
+                                <div className="flex gap-2 text-red-600 items-center cursor-pointer">
+                                    <TrashIcon className="w-4 h-4" /> Delete
+                                </div>
+                            )}
+                        </DropdownMenuItem>
+                    </Protect>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
+    );
+}
--- src/app/(dashboard)/_components/file-browser.tsx
+++ src/app/(dashboard)/_components/file-browser.tsx
@@ -5,10 +5,13 @@ import { useQuery } from 'convex/react';
 import { api } from '../../../../convex/_generated/api';
 import { UploadButton } from './upload-button';
 import { FileCard } from './file-card';
-import { Loader2 } from 'lucide-react';
+import { GridIcon, Loader2, RowsIcon, TableIcon } from 'lucide-react';
 import Image from 'next/image';
 import { useState } from 'react';
 import { SearchBar } from './search-bar';
+import { DataTable } from './file-table';
+import { columns } from './columns';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 function Placeholder() {
     return (
@@ -55,40 +58,62 @@ export function FileBrowser({
     );
     const isLoading = files === undefined;
 
+    const modifiedFiles =
+        files?.map((file) => ({
+            ...file,
+            isFavorited: (favorites ?? []).some(
+                (favorite) => favorite.fileId === file._id
+            )
+        })) ?? [];
+
     return (
         <div>
-            {isLoading && (
-                <div className="flex flex-col gap-8 w-full items-center mt-24">
-                    <Loader2 className="h-32 w-32 animate-spin text-gray-500" />
-                    <div className="text-2xl">Loading your images...</div>
-                </div>
-            )}
+            <div className="flex justify-between items-center mb-8">
+                <h1 className="text-4xl font-bold">{title}</h1>
 
-            {!isLoading && (
-                <>
-                    <div className="flex justify-between items-center mb-8">
-                        <h1 className="text-4xl font-bold">{title}</h1>
+                <SearchBar query={query} setQuery={setQuery} />
 
-                        <SearchBar query={query} setQuery={setQuery} />
+                <UploadButton />
+            </div>
 
-                        <UploadButton />
+            <Tabs defaultValue="grid">
+                <TabsList className="mb-2">
+                    <TabsTrigger
+                        value="grid"
+                        className="flex gap-2 items-center"
+                    >
+                        <GridIcon />
+                        Grid
+                    </TabsTrigger>
+                    <TabsTrigger
+                        value="table"
+                        className="flex gap-2 items-center"
+                    >
+                        <RowsIcon /> Table
+                    </TabsTrigger>
+                </TabsList>
+
+                {isLoading && (
+                    <div className="flex flex-col gap-8 w-full items-center mt-24">
+                        <Loader2 className="h-32 w-32 animate-spin text-gray-500" />
+                        <div className="text-2xl">Loading your files...</div>
                     </div>
+                )}
 
-                    {files.length === 0 && <Placeholder />}
-
+                <TabsContent value="grid">
                     <div className="grid grid-cols-3 gap-4">
-                        {files?.map((file) => {
-                            return (
-                                <FileCard
-                                    key={file._id}
-                                    file={file}
-                                    favorites={favorites ?? []}
-                                />
-                            );
+                        {modifiedFiles?.map((file) => {
+                            return <FileCard key={file._id} file={file} />;
                         })}
                     </div>
-                </>
-            )}
+                </TabsContent>
+                <TabsContent value="table">
+                    {' '}
+                    <DataTable columns={columns} data={modifiedFiles} />
+                </TabsContent>
+            </Tabs>
+
+            {files?.length === 0 && <Placeholder />}
         </div>
     );
 }
